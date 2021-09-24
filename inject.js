c1fead2b41aad950ce1fac639fc4dac35aea872b
Bao Ho Nguyen <baohn2@vng.com.vn>
2021-09-24 14:47:34 +0700

    updated

--- inject.js
+++ inject.js
@@ -5,4 +5,43 @@ function injectScript(file, node) {
   s.setAttribute("src", file);
   th.appendChild(s);
 }
+function injectButton(text, node) {
+  var th = document.getElementsByTagName(node)[0];
+  var s = document.createElement("button");
+  s.innerText  =  text;
+  s.setAttribute("style", `    position: fixed;
+  top: 10vh;
+  font-weight: bold;
+  padding: 10px 20px;
+  right: 2vw;
+  background: #377988;
+  z-index:9999;
+  outline: none;
+  border: none;
+  border: 1px dashed yellow;
+  color: white;
+  cursor: pointer;`);
+  s.setAttribute("id", "actionButton");
+  th.appendChild(s);
+}
+function injectLabel(text, node) {
+  var th = document.getElementsByTagName(node)[0];
+  var s = document.createElement("span");
+  s.innerText  =  text;
+  s.setAttribute("style", `position: fixed;
+  top: 15vh;
+  font-weight: bold;
+  padding: 10px 20px;
+  right: 2vw; 
+  z-index:9999;
+  outline: none;
+  border: none;
+  border: 1px dashed yellow;
+  color: maroon;
+  cursor: pointer;`);
+  s.setAttribute("id", "actionLabel");
+  th.appendChild(s);
+}
 injectScript(chrome.runtime.getURL("runner.js"), "body");
+injectButton("SCRAPE NOW", "body");
+injectLabel("Status", "body");
--- runner.js
+++ runner.js
@@ -1,90 +1,326 @@
-console.log("runner 1")
-location.href.includes(".uk/properties/") && window.addEventListener("load", async function () {
-  while (!window.PAGE_MODEL) {
-    await sleep(2000);
-  }
-  const addressElement = document.querySelector('h1[itemProp="streetAddress"]');
-  /*UPDATE LOADING*/
-  const originalAddress = addressElement.innerText;
-  addressElement.innerText = "... " + originalAddress;
-  /*MAIN*/
-  const address = window.PAGE_MODEL.propertyData.address;
-  !address.deliveryPointId &&
-    (addressElement.innerText = "ⓧ " + originalAddress);
-  const post_code = address.outcode + "-" + address.incode;
-  const dataHistory = await getHistoryOfHouse(address);
-  !dataHistory.soldPropertyTransactionHistories &&
-    dataHistory.soldPropertyTransactionHistories.length === 0 &&
-    (addressElement.innerText = "ⓧ " + originalAddress);
-  const houseTransaction = dataHistory.soldPropertyTransactionHistories
-    .map((transaction) => transaction.soldPrice + "_" + transaction.year)
-    .join("-");
-  houseTransaction.trim().length === 0 &&
-    (addressElement.innerText = "ⓧ " + originalAddress);
+/** SCRAPER_DATA
+ * currentIndex
+ * status running - init
+ * data
+ */
 
-  console.log(houseTransaction);
-  const rawLocationHtml = await getLocationIDRaw(post_code);
-  const locId = /POSTCODE\^(\d+)/.exec(rawLocationHtml)[1];
-  const firstPageData = await getHistoryOfLocation(locId);
-  const pageNum = firstPageData.pagination.last;
-  const rawTransactions = firstPageData.results.properties;
-  for (var i = 2; i <= pageNum; i++) {
-    rawTransactions.push((await getHistoryOfLocation(i)).results.properties);
+const jsonAPI = "http://jsonblob.com/api/jsonBlob/890821801681371136";
+console.log("runner");
+(async () => {
+  while (!document.getElementById("actionButton")) {
+    await sleep(1000);
   }
-  const transactions = rawTransactions.map((item) => ({
-    address: item.address,
-    transaction:
-      (item.transactions &&
-        item.transactions
-          .map(
-            (transaction) =>
-              transaction.displayPrice +
-              "_" +
-              transaction.dateSold.split(" ")[2]
-          )
-          .join("-")) ||
-      "",
-  }));
-  const matchTransaction = transactions.find(
-    (transaction) => houseTransaction.trim() === transaction.transaction
+})();
+const onClickScrape = async () => {
+  console.log("clicked scraping");
+  let data = await getLocalData();
+
+  data = { currentIndex: 0, status: "init", merchants: [], orders: [] };
+  await setLocalData(data);
+  await init();
+};
+
+const getLocalData = () => {
+  return new Promise((resolve, reject) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://jsonblob.com/api/jsonBlob/890836991147786240",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => resolve(result))
+      .catch((error) => console.log("error", error));
+  });
+};
+const setLocalData = (data) => {
+  return new Promise((resolve, reject) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: JSON.stringify(data),
+      redirect: "follow",
+    };
+    fetch(
+      "https://jsonblob.com/api/jsonBlob/890836991147786240",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => resolve(result))
+      .catch((error) => console.log("error", error));
+  });
+};
+
+const localStorageHandler = async () => {
+  const data = await getLocalData();
+  console.log(data);
+  switch (data.status) {
+    case "running":
+      running();
+      break;
+  }
+};
+//SE@656%&#
+const running = async () => {
+  const data = await getLocalData();
+  console.log("currentData", data);
+  const merchant = data.merchants[data.currentIndex];
+  if (!merchant) {
+    data.status = "init";
+  } else {
+    const orders = await getAllOrders(merchant);
+    data.orders.push(
+      ...orders.map((order) => ({
+        ...order,
+        accountName: merchant.name,
+        marketName: merchant.marketplaceName,
+      }))
+    );
+    data.currentIndex++;
+    await setLocalData(data);
+    if (data.merchants.length === data.currentIndex + 1) {
+    } else switchToMerchant(data.merchants[data.currentIndex]);
+  }
+};
+const switchToMerchant = (merchant) => {
+  console.log(
+    "switching to ...",
+    getSwitchRegionUrl(
+      merchant.marketplaceName,
+      merchant.directedId,
+      merchant.merchantDirectId,
+      merchant.marketplaceId
+    )
   );
-  console.log(transactions);
-  console.log(matchTransaction);
-  /*UPDATE RESULT*/
-  matchTransaction &&
-    matchTransaction.address &&
-    (addressElement.innerText = matchTransaction.address);
-});
-const getLocationIDRaw = (postcode) => {
-  console.log("fetching");
-  return new Promise((resolve, reject) => {
-    fetch(`https://www.rightmove.co.uk/house-prices/${postcode}`)
-      .then((res) => res.text())
-      .then((data) => resolve(data));
-  });
+  window.location.href = getSwitchRegionUrl(
+    merchant.marketplaceName,
+    merchant.directedId,
+    merchant.merchantDirectId,
+    merchant.marketplaceId
+  );
+  // window.location.href = getSwitchRegionUrl(merchant.marketplaceName, merchant.directedId, merchant.merchantDirectId, merchant.marketplaceId);
 };
-const getHistoryOfHouse = ({ deliveryPointId }) => {
-  console.log("fetching");
+const init = async () => {
+  const data = await getLocalData();
+  const merchants = (await getMerchants()).partnerAccounts;
+  const parsedMerchants = [].concat
+    .apply(
+      [],
+      merchants.map((merchant) =>
+        merchant.merchantMarketplaces.map((market) => ({
+          ...market,
+          merchantDirectId: market.directedId,
+          directedId: merchant.directedId,
+          name: merchant.name,
+        }))
+      )
+    )
+    .filter((item) =>
+      ["United States", "United Kingdom", "India"].includes(
+        item.marketplaceName.trim()
+      )
+    );
+  data.merchants = parsedMerchants;
+  data.status = "running";
+  await setLocalData(data);
+  await sleep(3000);
+  switchToMerchant(data.merchants[0]);
+};
+
+const region = {
+  US: {
+    domain: "https://sellercentral.amazon.com/",
+  },
+  UK: {
+    domain: "https://sellercentral.amazon.co.uk/",
+  },
+  IN: {
+    domain: "https://sellercentral.amazon.in/",
+  },
+};
+const getDomain = () => {
+  if (location.href.includes(".amazon.com")) return region.US.domain;
+  else if (location.href.includes(".amazon.co.uk")) return region.UK.domain;
+  else return region.IN.domain;
+};
+const getDomainByRegion = (regionName) => {
+  switch (regionName) {
+    case "United States":
+      return region.US.domain;
+    case "United Kingdom":
+      return region.UK.domain;
+    default:
+      return region.IN.domain;
+  }
+};
+const getSwitchRegionUrl = (
+  regionName,
+  directId,
+  merchantDirectId,
+  marketplaceId
+) => {
+  return (
+    getDomainByRegion(regionName) +
+    `/home?mons_sel_dir_mcid=${merchantDirectId}&mons_sel_mkid=${marketplaceId}&mons_sel_dir_paid=amzn1.pa.d.${directId}`
+  );
+};
+
+const getMerchants = () => {
   return new Promise((resolve, reject) => {
     fetch(
-      `https://www.rightmove.co.uk/properties/api/soldProperty/transactionHistory/${deliveryPointId}`
+      getDomain() +
+        "global-picker/data/get-merchant-marketplaces-with-no-partner-account-access?stck=eu",
+      {
+        headers: {
+          accept: "*/*",
+          "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+          "cache-control": "no-cache",
+          "content-type": "application/json",
+          pragma: "no-cache",
+          "sec-ch-ua":
+            '"Microsoft Edge";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-requested-with": "XMLHttpRequest",
+        },
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: '{"limitResults":true}',
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+      }
     )
-      .then((res) => res.json())
+      .then((data) => data.json())
       .then((data) => resolve(data));
   });
 };
-const getHistoryOfLocation = (locId) => {
-  console.log("fetching");
+//Mass order getters
+const m_getOrderDetail = async (orders) => {
+  const result = [];
+  for (let i = 0; i < orders.length; i++) {
+    const order = orders[i];
+    result.push({...order, address: (await getAddressDetail((await getOrderDetail(order.amazonOrderId)).order.blob))[order.amazonOrderId]});
+  }
+  return result;
+};
+const getAllOrders = async () => {
+  let offset = 0;
+  let result = [];
+  let orders = (await getOrders(offset)).orders;
+  result.push(...(await m_getOrderDetail(orders)));
+  while (orders.length) {
+    offset += 15;
+    orders = (await getOrders(offset)).orders;
+    result.push(...(await m_getOrderDetail(orders)));
+  }
+  return result;
+};
+const getOrders = (offset) => {
   return new Promise((resolve, reject) => {
     fetch(
-      `https://www.rightmove.co.uk/house-prices/result?locationType=POSTCODE&locationId=${locId}&page=1`
+      getDomain() +
+        `/orders-api/search?limit=15&offset=${offset}&sort=ship_by_asc&date-range=last-7&fulfillmentType=mfn&orderStatus=unshipped&forceOrdersTableRefreshTrigger=false`,
+      {
+        headers: {
+          accept: "application/json",
+          "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+          "anti-csrftoken-a2z-request": "true",
+          "content-type": "application/json",
+          "sec-ch-ua":
+            '"Microsoft Edge";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+        },
+        referrer:
+          "https://sellercentral.amazon.in/orders-v3/ref=xx_myo_dnav_xx?page=1",
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: null,
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+      }
     )
-      .then((res) => res.json())
+      .then((data) => data.json())
       .then((data) => resolve(data));
   });
 };
+const getAddressDetail = (blob) => {
+  return new Promise((resolve, reject) => {
+    fetch("https://sellercentral.amazon.in/orders-st/resolve", {
+      headers: {
+        accept: "application/json",
+        "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+        "content-type": "application/json",
+        "sec-ch-ua":
+          '"Microsoft Edge";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+      },
+      referrer:
+        "https://sellercentral.amazon.in/orders-v3/order/405-1856318-3832328",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: `{\"blobs\":[\"${blob}\"]}`,
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    })
+      .then((data) => data.json())
+      .then((data) => resolve(data));
+  });
+};
+const getOrderDetail = (id) => {
+  return new Promise((resolve, reject) => {
+    fetch(getDomain() + `/orders-api/order/${id}`, {
+      headers: {
+        accept: "application/json",
+        "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+        "anti-csrftoken-a2z-request": "true",
+        "content-type": "application/json",
+        "sec-ch-ua":
+          '"Microsoft Edge";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+      },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+    })
+      .then((data) => data.json())
+      .then((data) => resolve(data));
+  });
+};
+
 const sleep = (ms) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve(), ms);
   });
 };
+
+/*MAIN*/
+
+document.getElementById("actionButton").onclick = onClickScrape;
+localStorageHandler();
